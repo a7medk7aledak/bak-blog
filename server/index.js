4a63b14563818f1c7dd67d2005b5bfae48e897dc
Ahmed Khaled <92623979+a7medk7aledak@users.noreply.github.com>
2024-08-17 14:44:07 +0300

    add middleware

--- server/index.js
+++ server/index.js
@@ -8,14 +8,27 @@ const PORT = process.env.PORT || 8000;
 
 import path from "path";
 const app = express();
-// to can use json when front-end send request  json
-app.use(express.json())
+// to can use json when front-end send request json
+app.use(express.json());
+
 // Connect Database
 connectDB();
 
 //run my back-end server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-//middleware
+//routes and middleware
 app.use("/api/user", userRoutes); 
 app.use("/api/auth", authRoutes);
+
+//middleware to make error is butter
+app.use((err, req, res, next) => {
+    //error must have status code if not have include 500
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
