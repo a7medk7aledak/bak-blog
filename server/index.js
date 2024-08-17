eb8cc3bd9ef15d1a10d2dc2768b426ebb938dccb
Ahmed Khaled <92623979+a7medk7aledak@users.noreply.github.com>
2024-08-17 13:07:38 +0300

    create auth

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import connectDB from "./config/db.js";
-import userRoutes from './routes/user.route.js'
+import userRoutes from './routes/user.route.js';
+import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
 import path from "path";
 const app = express();
+// to can use json when front-end send request  json
+app.use(express.json())
 // Connect Database
 connectDB();
 
 //run my back-end server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-app.use("/api/user", userRoutes); //use to middleware
+//middleware
+app.use("/api/user", userRoutes); 
+app.use("/api/auth", authRoutes);
