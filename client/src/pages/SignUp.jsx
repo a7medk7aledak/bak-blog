4c80a4fe2eea20a748b41f6826d5a547bcdc45db
Ahmed Khaled <92623979+a7medk7aledak@users.noreply.github.com>
2024-08-19 11:01:03 +0300

    client

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import { Button, Label, TextInput } from "flowbite-react";
-import React, { useState } from "react";
+import  { useState } from "react";
 import { Link } from "react-router-dom";
 import { OAuth } from "../components/OAuth";
 
