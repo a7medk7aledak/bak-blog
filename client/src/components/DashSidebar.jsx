350d81ed20d4fabb7817ffa9e481dd7ba648a208
Belal Ahmed <55669680+belalwws@users.noreply.github.com>
2024-08-19 16:44:22 +0300

    dashsidebar

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,14 +1,31 @@
 import { Sidebar } from "flowbite-react";
+import { useEffect, useState } from "react";
 import { HiUser, HiArrowSmRight } from "react-icons/hi";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const DashSidebar = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup className="flex flex-col gap-1">
           <Link to="/dashboard?tab=profile">
-            <Sidebar.Item icon={HiUser} labelColor="dark" as="div">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              labelColor="dark"
+              as="div"
+              label="user"
+            >
               Profile
             </Sidebar.Item>
           </Link>
