f6659251fa447fbeb905235828cb8f072c7717ac
Belal Ahmed <55669680+belalwws@users.noreply.github.com>
2024-09-01 18:28:42 +0300

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -116,9 +116,6 @@ export const Header = () => {
         <Navbar.Link active={path === "/about"} as={"div"}>
           <Link to="/about">About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/projects"} as={"div"}>
-          <Link to="/projects">Projects</Link>
-        </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   );
